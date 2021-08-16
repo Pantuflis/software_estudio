a690a4ce8cb1da858421f30e60150cd14787d0a5
Roberto <roberto_db@outlook.com.ar>
2021-08-16 04:39:41 -0300

    gui terminada falta conectar las funciones

--- gui/elements.py
+++ gui/elements.py
@@ -1,45 +1,119 @@
 import sys
-from PyQt6.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget, QFileDialog, QGridLayout, QFrame, QMainWindow, QCheckBox
+from PyQt6.QtWidgets import QApplication, QGroupBox, QLabel, QPushButton, QWidget, QFileDialog, QGridLayout, QLineEdit, QProgressBar, QRadioButton, QVBoxLayout, QGroupBox
 from PyQt6.QtGui import QPixmap
 from PyQt6 import QtGui, QtCore
 from PyQt6.QtGui import QCursor
 from PyQt6 import QtWidgets
 
 
-class Button(QPushButton):
-    def __init__(self, btn_name=""):
-        super().__init__(btn_name)
-        self.setCursor(
-            QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
-        self.setStyleSheet(
-            """*{border: 4px solid 'green';
-            border-radius: 35px;
-            font-size: 35px;
-            color: 'white';
-            padding: 10px 0;
-            margin: 100px 100px;}
-            *:hover{background: 'green'}
-            *:pressed{background: 'lightgreen'}"""
-        )
-        # self.clicked.connect(self.check)
-
-    def check(self, option):
-        option()
+class App(QApplication):
+    def __init__(self):
+        super().__init__()
 
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Pantuflis Software")
-        self.setFixedWidth(1000)
-        self.setStyleSheet("background: 'black';")
-        self.grid = QGridLayout()
-        self.setLayout(self.grid)
+        self.setFixedSize(500, 300)
+        self.setStyleSheet("background: '#3b444b';")
+        # Change logo fir definitive
+        self.setWindowIcon(QtGui.QIcon('logo.png'))
+        self.global_layout = QVBoxLayout()
+        self.op_container = QGroupBox("Options")
+        self.op_container_layout = QGridLayout()
+        self.op_container.setLayout(self.op_container_layout)
+        self.op_container.setStyleSheet(
+            """*{border: 1.5px solid '#0e1111';
+            border-radius: 4px;
+            color: 'white';
+            font-size: 15px;
+            margin: 5px 0 0 0;
+            padding: 0 10px 0 10px;}
+            *::title {
+                top: -8px;
+                left: 10px;
+                }"""
+        )
+        self.options_layout = QGridLayout()
+        self.process_layout = QGridLayout()
+        self.global_layout.setSpacing(0)
+        self.global_layout.setContentsMargins(5, 10, 5, 10)  # L, T, R, B
+        self.op_container_layout.setSpacing(0)
+        self.op_container_layout.setContentsMargins(5, 0, 5, 0)
+        self.options_layout.setSpacing(0)
+        self.options_layout.setContentsMargins(0, 0, 0, 0)
+        self.process_layout.setSpacing(0)
+        self.process_layout.setContentsMargins(0, 15, 0, 10)
+        self.process_layout.setSpacing(20)
+        self.setLayout(self.global_layout)
 
 
-# System
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
-    sys.exit(app.exec())
+class BrowseBar(QLineEdit):
+    def __init__(self):
+        super().__init__()
+        self.setFixedSize(375, 35)
+        self.setAlignment(QtCore.Qt.AlignmentFlag.AlignLeft)
+        self.setStyleSheet(
+            """border: 2px solid '#0e1111';
+            background-color: '#232b2b';
+            border-radius: 7px;
+            color: 'white';
+            font-size: 15px;
+            margin: 0;"""
+        )
+
+
+class Button(QPushButton):
+    def __init__(self, text="", width=100, height=100, radius='10', font_size='10', t_margin='0', b_margin='0', r_margin='0', l_margin='0'):
+        super().__init__()
+        self.setText(text)
+        self.setFixedSize(width, height)
+        self.setStyleSheet(
+            "*{border: 3px solid '#68a7ff';" +
+            f"border-radius: {radius}px;" +
+            "background: '#68a7ff';" +
+            "font-weight: bold;" +
+            "font-family: 'Arial';" +
+            f"font-size: {font_size}px;" +
+            "color: 'white';" +
+            f"margin-right: {r_margin}px;" +
+            f"margin-left: {l_margin}px;" +
+            f"margin-top: {t_margin}px;" +
+            f"margin-bottom: {b_margin}px;" +
+            "padding: 5px 5px;}" +
+            "*:hover{background: '#2f6ec5'}" +
+            "*:hover{border: '#2f6ec5'}" +
+            "*:pressed{background: '#1c4e94'}"
+        )
+
+
+class ProgressBar(QProgressBar):
+    def __init__(self):
+        super().__init__()
+        self.setStyleSheet(
+            """ProgressBar{border: 2px solid '#0e1111';
+            background-color: '#232b2b';
+            border-radius: 7px;
+            color: 'white';
+            font-size: 15px;
+            font-weight: bold;
+            text-align: center;}
+            *::chunk{
+                background-color: '#8237f9';
+                border-radius: 4px;
+                }"""
+        )
+
+
+class Options(QRadioButton):
+    def __init__(self, text=""):
+        super().__init__()
+        self.setText(text)
+        self.setContentsMargins(0, 0, 0, 0)
+        self.setStyleSheet(
+            """border: 0;
+            color: 'white';
+            margin: 0;
+            padding: 0;"""
+        )
--- gui/main.py
+++ gui/main.py
@@ -1,110 +1,51 @@
+import os
+import time
 import sys
-from PyQt6.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget, QFileDialog, QGridLayout, QFrame, QMainWindow, QCheckBox
-from PyQt6.QtGui import QPixmap
+from PyQt6.QtWidgets import QApplication
+from elements import App, MainWindow, BrowseBar, Button, ProgressBar, Options
 from PyQt6 import QtGui, QtCore
-from PyQt6.QtGui import QCursor
-from PyQt6 import QtWidgets
-from elements import MainWindow, Button
 
-
-widgets = {
-    "logo": [],
-    "button": [],
-    "score": [],
-    "question": [],
-    "answer1": [],
-    "answer2": [],
-    "answer3": [],
-    "answer4": [],
-}
-
-# Create window with black background
+# Create application adn window
 app = QApplication(sys.argv)
 window = MainWindow()
 
+# Create elements for window
+option_1 = Options('Archivo Grupos Integrados')
+option_2 = Options('SIFERE - Retenciones AGIP')
+option_3 = Options('SIFERE - Percepciones AGIP')
+option_4 = Options('Agente de Recaudacion Misiones')
+browser_bar = BrowseBar()
+browse_btn = Button('BROWSE', width=90, height=35, radius='10', font_size='12')
+convert_btn = Button('CONVERT', width=400, height=50,
+                     radius='10', font_size='25', l_margin='90', r_margin='0')
+progress_bar = ProgressBar()
 
-def start_game():
-    for widget in widgets:
-        if widgets[widget] != []:
-            widgets[widget][-1].hide()
-        for i in range(len(widgets[widget])):
-            widgets[widget].pop()
-    frame_2()
-
-
-def create_buttons(btn_name):
-    answer_button = Button(btn_name)
-    answer_button.setCursor(QCursor(QtCore.Qt.CursorShape.PointingHandCursor))
-    answer_button.setFixedWidth(485)
-    return answer_button
-
-
-def frame_1():
-    # Display logo
-    image = QPixmap("logo.png")
-    logo = QLabel()
-    logo.setPixmap(image)
-    logo.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
-    logo.setStyleSheet("margin-top: 50px")
-    widgets['logo'].append(logo)
-
-    # Buttons
-    ok_button = Button("OK")
-    ok_button.clicked.connect(start_game)
-    cancel_button = Button("Cancel")
-    widgets['button'].append(ok_button)
-
-    # Grid
-    window.grid.addWidget(widgets['logo'][-1], 0, 0, 1, 2)
-    window.grid.addWidget(widgets['button'][-1], 1, 0)
-    window.grid.addWidget(cancel_button, 1, 1)
+# Display element in window
+window.global_layout.addLayout(window.options_layout)
+window.global_layout.addWidget(window.op_container)
+window.op_container_layout.addWidget(option_1, 0, 0)
+window.op_container_layout.addWidget(option_2, 0, 1)
+window.op_container_layout.addWidget(option_3, 1, 0)
+window.op_container_layout.addWidget(option_4, 1, 1)
+window.global_layout.addLayout(window.process_layout)
+window.process_layout.addWidget(browser_bar, 0, 0)
+window.process_layout.addWidget(browse_btn, 0, 1)
+window.process_layout.addWidget(convert_btn, 1, 0, 1, 2)
+window.process_layout.addWidget(progress_bar, 2, 0, 2, 2)
 
 
-def frame_2():
-    score = QLabel('80')
-    score.setAlignment(QtCore.Qt.AlignmentFlag.AlignRight)
-    score.setStyleSheet(
-        """border: 1px solid 'green';
-           border-radius: 35px;
-           background-color: 'green'; 
-           font-size: 35px;
-           color: 'white';
-           padding: 20px 15px;
-           margin: 100px 200px;"""
-    )
-    widgets['score'].append(score)
+def run():
+    progress_bar.setRange(0, 10)
+    for i in range(100):
+        time.sleep(0.01)
+        progress_bar.setValue(i+1)
+        app.processEvents()
 
-    question = QLabel('Place holder')
-    question.setAlignment(QtCore.Qt.AlignmentFlag.AlignCenter)
-    question.setWordWrap(True)
-    question.setStyleSheet(
-        """font-family: Shanti;
-        font-size: 25px;
-        color: 'white';
-        padding: 75px;"""
-    )
-    widgets['question'].append(question)
 
-    button1 = create_buttons('answer1')
-    button2 = create_buttons('answer2')
-    button3 = create_buttons('answer3')
-    button4 = create_buttons('answer4')
-
-    widgets['answer1'].append(button1)
-    widgets['answer2'].append(button2)
-    widgets['answer3'].append(button3)
-    widgets['answer4'].append(button4)
-
-    window.grid.addWidget(widgets['score'][-1], 0, 1)
-    window.grid.addWidget(widgets['question'][-1], 1, 0, 1, 2)
-    window.grid.addWidget(widgets['question'][-1], 1, 0, 1, 2)
-    window.grid.addWidget(widgets['answer1'][-1], 2, 0)
-    window.grid.addWidget(widgets['answer2'][-1], 2, 1)
-    window.grid.addWidget(widgets['answer3'][-1], 3, 0)
-    window.grid.addWidget(widgets['answer4'][-1], 3, 1)
+# Functions
+convert_btn.clicked.connect(run)
 
 
 if __name__ == "__main__":
-    frame_1()
     window.show()
     sys.exit(app.exec())
